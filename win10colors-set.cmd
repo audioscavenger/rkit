526a7e13cfc0d32f607e6b731075f1cbf52cb3db
Laurent Derewonko <lderewonko@nqueue.com>
2019-06-10 17:15:09 -0700

    added win10colors-set.cmd

---
+++ win10colors-set.cmd
@@ -0,0 +1,45 @@
+@echo off
+set END=[0m
+set HIGH=[1m
+set Underline=[4m
+set REVERSE=[7m
+
+REM echo [101;93m NORMAL FOREGROUND COLORS [0m
+set k=[30m
+set r=[31m
+set g=[32m
+set y=[33m
+set b=[34m
+set m=[35m
+set c=[36m
+set w=[37m
+
+REM echo [101;93m NORMAL BACKGROUND COLORS [0m
+set RK=[40m
+set RR=[41m
+set RG=[42m
+set RY=[43m
+set RB=[44m
+set RM=[45m
+set RC=[46m
+set RW=[47m
+
+REM echo [101;93m STRONG FOREGROUND COLORS [0m
+set hK=[90m
+set hR=[91m
+set hG=[92m
+set hY=[93m
+set hB=[94m
+set hM=[95m
+set hC=[96m
+set hW=[97m
+
+REM echo [101;93m STRONG BACKGROUND COLORS [0m
+set hRK=[100m
+set hRR=[101m
+set hRG=[102m
+set hRY=[103m
+set hRB=[104m
+set hRM=[105m
+set hRC=[106m
+set hRW=[107m
