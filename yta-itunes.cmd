4baa5df0489b369ecceb612ca15a4329c10c7328
audioscavenger <dev@derewonko.com>
2026-01-23 09:45:38 -0700

    updated yta to use browser cookies

--- yta-itunes.cmd
+++ yta-itunes.cmd
@@ -1,10 +1,17 @@
 @echo OFF
 setlocal
+
+set ffmpeglocation=C:\wintools\multimedia\MediaCoder64\codecs64
+IF NOT EXIST %ffmpeglocation%\ set ffmpeglocation=E:\wintools\multimedia\MediaCoder64\codecs64
+IF NOT EXIST %ffmpeglocation%\ echo ERROR: ffmpeg not found & timeout /t 10 & exit /b 1
+
 :: downloads only audio from Youtube soundcloud and more https://github.com/yt-dlp/yt-dlp
 pushd "%USERPROFILE%\Music\iTunes\iTunes Media\Automatically Add to iTunes\"
 
+
 :defaults
-set ext=m4a
+set ext=mp3
+set BROWSER=firefox
 
 :main
 IF /I "%~1"==""       call :USAGE
@@ -30,9 +37,11 @@ REM pushd %~sdp1
 for /f "usebackq tokens=1,* delims==" %%a in (`type %1`) DO (
   REM echo IF "%%~a"=="URL" set url=%%b
   REM pause
+echo %%a
   IF "%%~a"=="URL" set url=%%b
 )
 
+echo call :url_dl "%url%"
 call :url_dl "%url%"
 goto :EOF
 
@@ -43,6 +52,8 @@ call :url_dl "%url%"
 goto :loop
 
 :url_dl
+set /P BROWSER=BROWSER? [%BROWSER%] 
+
 :: try to guess format
 REM echo %1 | findstr bandcamp.com >NUL && set format=mp3
 REM echo %1 | findstr bandcamp.com/track >NUL && set format=m4a
@@ -61,13 +72,14 @@ REM yt-dlp %1 --extractor-args youtube:player_client=android -x -S aext:mp3:m4a 
 :: falac over m4a because apparently falac is legin inside m4a
 REM yt-dlp %1 --extractor-args youtube:player_client=android -x -S aext:m4a:mp3 --no-keep-video --embed-metadata
 :: now it's perfect: mp3 first, then m4a aac because 99% of FM transmitters only play mp3/flac/wma/wav this is BS
-if %ext%==m4a %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 -S aext:m4a:mp3,acodec:aac:mp3 --no-keep-video --embed-metadata --add-metadata
+
+if %ext%==m4a %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 -S aext:m4a:mp3,acodec:aac:mp3 --no-keep-video --embed-metadata --add-metadata --cookies-from-browser %BROWSER%
 REM if %ext%==m4a %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 -S ext:mp4:m4a,aext:m4a:mp3,acodec:aac:mp3 --no-keep-video --embed-metadata --add-metadata --use-postprocessor FFmpegCopyStream --ppa CopyStream:"-c:v libx264 -c:a aac -f m4a"
 REM if %ext%==m4a %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 -S ext:mp4:m4a,aext:m4a:mp3,acodec:aac:mp3 --no-keep-video --embed-metadata --add-metadata --use-postprocessor FFmpegCopyStream --ppa CopyStream:"-c:v libx264 -c:a aac -f m4a"
-REM if %ext%==m4a  %~sdp0\yt-dlp %1 --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --cookies-from-browser edge --exec "C:\wintools\multimedia\MediaCoder64\codecs64\ffmpeg -i {} -c:v libx265 -c:a copy {}.h265.m4a"
+REM if %ext%==m4a  %~sdp0\yt-dlp %1 --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --cookies-from-browser %BROWSER% --exec "C:\wintools\multimedia\MediaCoder64\codecs64\ffmpeg -i {} -c:v libx265 -c:a copy {}.h265.m4a"
 REM if %ext%==m4a  %~sdp0\yt-dlp %1 --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --exec "C:\wintools\multimedia\MediaCoder64\codecs64\ffmpeg -i {} -c:a aac {}.m4a"
 REM if %ext%==m4a  %~sdp0\yt-dlp %1 --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --exec "C:\wintools\multimedia\MediaCoder64\codecs64\ffmpeg -i {} -c:a aac -vbr 4 {}.m4a"
-if %ext%==mp3  %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --audio-format mp3 --no-keep-video --embed-metadata --add-metadata
+if %ext%==mp3  %~sdp0\yt-dlp %1 -x --ffmpeg-location C:\wintools\multimedia\MediaCoder64\codecs64 --audio-format mp3 --no-keep-video --embed-metadata --add-metadata --cookies-from-browser %BROWSER%
 goto :EOF
 
 :USAGE
